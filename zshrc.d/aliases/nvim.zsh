d76c4fc500e6545c8374d488a382aeaf16867b67
awarewen-gpd <blacktonewascrazy@gmail.com>
2024-09-08 15:08:28 +0800

    迁移 zsh 配置

---
+++ zshrc.d/aliases/nvim.zsh
@@ -0,0 +1,13 @@
+# Nvim alias
+alias lnvim="NVIM_APPNAME=lnvim nvim"
+alias Snvim="NVIM_APPNAME=Snvim nvim"
+
+# Use neovide to connect nvim server mode
+alias nvimser="nvim --headless --listen localhost:7777"
+alias nvide="env -u WAYLAND_DISPLAY neovide --multigrid --server=localhost:7777"
+
+alias lazyvim="NVIM_APPNAME=LazyVim nvim"
+alias chadvim="NVIM_APPNAME=NvChad nvim"
+alias echavim="NVIM_APPNAME=EchaVim nvim"
+alias nvimdot="NVIM_APPNAME=nvimdots nvim"
+
